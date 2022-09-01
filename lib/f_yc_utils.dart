52ec0427e2ea48ff08b19594019a0d2901ee631d
yhfwork <mm20102010mm>
2022-09-01 22:12:04 +0800

    init

--- lib/f_yc_utils.dart
+++ lib/f_yc_utils.dart
@@ -3,3 +3,4 @@ library f_yc_utils;
 export 'src/index.dart';
 export 'package:flutter_easyloading/flutter_easyloading.dart';
 export 'package:get/get.dart';
+export 'package:flustars_flutter3/flustars_flutter3.dart';
