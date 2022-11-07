2f7407ac653f6c42096da61784a8d12330558791
yhfwork <mm20102010mm>
2022-11-08 02:28:51 +0800

    Update f_yc_utils.dart

--- lib/f_yc_utils.dart
+++ lib/f_yc_utils.dart
@@ -1,8 +1,14 @@
 library f_yc_utils;
 
 export 'package:get/get.dart';
-export 'package:get/get_connect/http/src/request/request.dart';
 export 'package:crypto/crypto.dart';
+export 'package:battery_plus/battery_plus.dart';
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:device_info_plus/device_info_plus.dart';
+export 'package:package_info_plus/package_info_plus.dart';
+export 'package:safe_device/safe_device.dart';
+export 'package:uuid_helper/uuid_helper.dart';
+
 export './src/f_yc_color.dart';
 export './src/f_yc_date.dart';
 export './src/f_yc_focus.dart';
